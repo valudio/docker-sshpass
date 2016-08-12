1407f9c0b08bb6535c7292901e76b0a1dff2a5ba
Roberto Huertas <roberto.huertas@outlook.com>
2016-08-12 20:51:58 +0200

    support for special dumps

--- alpine-mongo-import/import.sh
+++ alpine-mongo-import/import.sh
@@ -1,30 +1,54 @@
 #!/bin/sh
 # vars ##############################
-# FROM=$1
-# TO=$2
-# PWORD=$3
-# PATHDATA=$4
-# DB=$5
+# FROM
+# TO
+# PWORDFROM
+# PWORDTO
+# DB
+# OP [GETDUMP, TRANSFER(default)]
+# DUMPUSER=$6 (only needed if GETDUMP -> <USERNAME_TO_LOGIN>)
+# DUMPPATH=$6 (only needed if GETDUMP -> <PATH_TO_STORE>)
 # script ############################
-echo "Importing from $FROM to $TO with pwd: $PWORD to path $PATHDATA and database $DB"
-echo "Import data from $FROM to $TO"
-echo "Dumping data from $FROM"
-sshpass -p $PWORD ssh -o StrictHostKeyChecking=no root@$FROM << EOF
+
+PWDTO=$PWORDFROM
+
+if [ -z "$PWORDTO"]; then
+PWDTO=$PWORDFROM
+echo "[No PWDTO present. Assigning '$PWDTO']"
+fi
+
+echo "[Importing from $FROM to $TO with pwd: $PWORD, database: $DB, dumpUser: $DUMPUSER, dumpPath: $DUMPPATH]"
+
+echo "[Dumping data from $FROM  ............................]"
+sshpass -p $PWORDFROM ssh -o StrictHostKeyChecking=no root@$FROM << EOF
 docker exec -d mongodb bash -c "rm -rf /data/db/$DB && mongodump -d $DB -o /data/db"
 exit
 EOF
-echo "Copying data from $FROM into local"
+
+echo "[Copying data from $FROM into temporal folder ............................]"
 mkdir ~/tmpDump
-sshpass -p $PWORD scp root@$FROM:$PATHDATA/$DB/* ~/tmpDump
-echo "Transferring data to $TO"
-# removing old data
-sshpass -p $PWORD ssh -o StrictHostKeyChecking=no root@$TO "rm -rf $PATHDATA/$DB && mkdir $PATHDATA/$DB"
-sshpass -p $PWORD scp ~/tmpDump/* root@$TO:$PATHDATA/$DB
-echo "Removing temporary data from local"
-rm -rf ~/tmpDump
-echo "Restoring data in $TO"
-sshpass -p $PWORD ssh -o StrictHostKeyChecking=no root@$TO << EOF
+sshpass -p $PWORDFROM scp root@$FROM:/var/lib/mongodb/$DB/* ~/tmpDump
+
+echo "[Is this dump? => $OP]"
+
+if [ "$OP" == "GETDUMP" ]; then
+echo "[Removing old data from $TO ..........................]"
+sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no $DUMPUSER@$TO "rm -rf $DUMPPATH && mkdir $DUMPPATH"
+echo "[Transferring dump to $DUMPPATH ............................]"
+sshpass -p $PWDTO scp ~/tmpDump/* $DUMPUSER@$TO:$DUMPPATH
+else
+echo "[Removing old data from $TO ..........................]"
+sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no root@$TO "rm -rf /var/lib/mongodb/$DB && mkdir /var/lib/mongodb/$DB"
+echo "[Transferring data to $TO ............................]"
+sshpass -p $PWDTO scp ~/tmpDump/* root@$TO:/var/lib/mongodb/$DB
+echo "[Restoring data in $TO ...............................]"
+sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no root@$TO << EOF
 docker exec -d mongodb bash -c "mongorestore --db $DB /data/db/$DB"
 exit
 EOF
-echo "Done!" 
+fi
+
+echo "[Removing temporary data ...............................]"
+rm -rf ~/tmpDump
+
+echo "[Done!]" 
