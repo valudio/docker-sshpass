cfd4950e21beb5ec47f3df1f66491e2c7471f1ca
Roberto Huertas <roberto.huertas@outlook.com>
2016-08-16 13:58:41 +0200

    new params

--- alpine-mongo-import/import.sh
+++ alpine-mongo-import/import.sh
@@ -11,13 +11,20 @@
 # script ############################
 
 PWDTO=$PWORDFROM
+PATH_TO_STORE=$DUMPPATH
 
 if [ -z "$PWORDTO"]; then
 PWDTO=$PWORDFROM
 echo "[No PWDTO present. Assigning '$PWDTO']"
 fi
 
-echo "[Importing from $FROM to $TO with pwd: $PWORD, database: $DB, dumpUser: $DUMPUSER, dumpPath: $DUMPPATH]"
+if [ -z "$DUMPPATH"]; then
+PATH_TO_STORE="/var/lib/mongodb"
+echo "[No DUMPPATH present. Assigning '/var/lib/mongodb']"
+fi
+
+
+echo "[Importing from $FROM to $TO with pwd_FROM: $PWORDFROM, pwd_TO: $PWORDTO, database: $DB, dumpUser: $DUMPUSER, dumpPath: $DUMPPATH]"
 
 echo "[Dumping data from $FROM  ............................]"
 sshpass -p $PWORDFROM ssh -o StrictHostKeyChecking=no root@$FROM << EOF
@@ -33,14 +40,14 @@ echo "[Is this dump? => $OP]"
 
 if [ "$OP" == "GETDUMP" ]; then
 echo "[Removing old data from $TO ..........................]"
-sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no $DUMPUSER@$TO "rm -rf $DUMPPATH && mkdir $DUMPPATH"
-echo "[Transferring dump to $DUMPPATH ............................]"
-sshpass -p $PWDTO scp ~/tmpDump/* $DUMPUSER@$TO:$DUMPPATH
+sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no $DUMPUSER@$TO "rm -rf $PATH_TO_STORE/$DB && mkdir $PATH_TO_STORE/$DB"
+echo "[Transferring dump to $PATH_TO_STORE ............................]"
+sshpass -p $PWDTO scp ~/tmpDump/* $DUMPUSER@$TO:$PATH_TO_STORE/$DB
 else
 echo "[Removing old data from $TO ..........................]"
-sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no root@$TO "rm -rf /var/lib/mongodb/$DB && mkdir /var/lib/mongodb/$DB"
+sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no root@$TO "rm -rf $PATH_TO_STORE/$DB && mkdir $PATH_TO_STORE/$DB"
 echo "[Transferring data to $TO ............................]"
-sshpass -p $PWDTO scp ~/tmpDump/* root@$TO:/var/lib/mongodb/$DB
+sshpass -p $PWDTO scp ~/tmpDump/* root@$TO:$PATH_TO_STORE/$DB
 echo "[Restoring data in $TO ...............................]"
 sshpass -p $PWDTO ssh -o StrictHostKeyChecking=no root@$TO << EOF
 docker exec -d mongodb bash -c "mongorestore --db $DB /data/db/$DB"
